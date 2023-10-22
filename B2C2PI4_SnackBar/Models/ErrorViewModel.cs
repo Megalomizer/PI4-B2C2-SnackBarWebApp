25f692cf759b22e03324d51ca5b3436718d7eba1
Megalomizer <Levifunk01@gmail.com>
2023-10-23 00:30:02 +0200

    Newly Created Project

---
+++ B2C2PI4_SnackBar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace B2C2PI4_SnackBar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
